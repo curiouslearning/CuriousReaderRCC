272c89833891113996e398101c269aed36cd75fb
redpix <dzavashviligeorge@gmail.com>
2019-05-24 09:06:30 +0400

    added book cover translation class for storing book covers

---
+++ CuriousReader/Assets/Scripts/Data/BookCoverTranslation.cs
@@ -0,0 +1,11 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public class BookCoverTranslation
+{
+    [SerializeField]
+    public ReaderLanguage   BookLanguage;
+    [SerializeField]
+    public Sprite           BookCoverSprite;
+}
