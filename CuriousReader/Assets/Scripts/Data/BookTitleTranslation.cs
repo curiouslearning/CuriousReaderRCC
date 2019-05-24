17fac5c4a41bfe76b2ffdb624e0d6762ad99d45f
redpix <dzavashviligeorge@gmail.com>
2019-05-24 09:07:21 +0400

    added book level to booktitletranslation class and renamed name field

---
+++ CuriousReader/Assets/Scripts/Data/BookTitleTranslation.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public class BookTitleTranslation 
+{
+    [SerializeField]
+    public int              BookLevel;
+    [SerializeField]
+    public ReaderLanguage   BookLanguage;
+    [SerializeField]
+    public string           AssetBundleBookFileName;
+}
