fb9d3907888ff7a0ab60aa92864bf7657e93b1de
redpix <dzavashviligeorge@gmail.com>
2019-05-29 21:38:16 +0400

    much simplified shelflevelbutton with only book level and color data

--- CuriousReader/Assets/Scripts/Shelf/ShelfBookLevelButton.cs
+++ CuriousReader/Assets/Scripts/Shelf/ShelfBookLevelButton.cs
@@ -1,61 +1,36 @@
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
-using System.Collections.Generic;
-using System.Linq;
 
-[RequireComponent(typeof(Image))]
-[RequireComponent(typeof(Button))]
-[RequireComponent(typeof(RectTransform))]
+[RequireComponent(typeof(Animator))]
 public class ShelfBookLevelButton : MonoBehaviour {
 
     #region Public Members
     
     public event UnityAction<ShelfBookLevelButton>    OnClick = delegate{};
 
-    [Header("Data")]
-    public List<BookTitleTranslation>   BookFileNameTranslations;
-    public Color    LevelColor;
-    
+    public int BookLevel { get { return m_bookLevel;  } }
+    public Color LevelColor { get { return m_levelColor; } }
+
     #endregion
 
     #region Private Members
 
-    [Header("Button components")]
+    [Header("Data")]
+    [SerializeField]
+    private int             m_bookLevel;
+    [SerializeField]
+    private Color           m_levelColor;
+
+    [Header("Components")]
     [SerializeField]
     private Button          m_button;
     [SerializeField]
-    private Image           m_backgroundImage;
+    private Animator        m_animator;
     [SerializeField]
-    private RectTransform   m_rectTransform;
-
+    private string          m_activateAnimationName;
     [SerializeField]
-    private Color           m_imageColorWhenActive;
-    [SerializeField]
-    private Vector2         m_scaleWhenActive;  // x,y = 1.4f recommended
-    [SerializeField]
-    private Color           m_imageColorWhenInactive;
-    [SerializeField]
-    private Vector2         m_scaleWhenInactive;
-
-    [Header("Level graphic components")]
-    [SerializeField]
-    private RectTransform   m_graphicsRectTransform;
-    [SerializeField]
-    private Vector2         m_graphicsScaleWhenActive;
-    [SerializeField]
-    private Vector2         m_graphicsScaleWhenInactive;
-
-
-    // Orange - FDBE00  // TODO:
-    // Red - FB390D
-    // Green - 1EA8A5
-
-    // Scales when clicked
-    // 1.4
-    // 0.8
-
-    private Dictionary<ReaderLanguage, string> m_bookFileNameTranslationsDictionary;
+    private string          m_deactivateAnimationName;
 
     #endregion
 
@@ -63,36 +38,24 @@ public class ShelfBookLevelButton : MonoBehaviour {
 
     public void Initialize()
     {
-        // To have easier and faster access to the list without needing to use a loop
-        m_bookFileNameTranslationsDictionary = BookFileNameTranslations
-            .ToDictionary(val => val.BookLanguage, val => val.BookAssetBundleFileName);
+        if (m_button == null)
+            m_button = GetComponent<Button>();
+        if (m_animator == null)
+            m_animator = GetComponent<Animator>();
+
         m_button.onClick.AddListener(() => { OnClick(this); });
     }
 
-    public string GetBookTitleForLanguage(ReaderLanguage language)
-    {
-        if (m_bookFileNameTranslationsDictionary.ContainsKey(language)) 
-        {
-           return  m_bookFileNameTranslationsDictionary[language];
-        } else
-        {
-            return "";
-        }
-    }
-    
     public void Activate()
     {
-        m_rectTransform.localScale = m_scaleWhenActive;
-        m_backgroundImage.color = m_imageColorWhenActive;
-        m_graphicsRectTransform.localScale = m_graphicsScaleWhenActive;
+        m_animator.Play(m_activateAnimationName);
     }
 
     public void Deactivate()
     {
-        m_rectTransform.localScale = m_scaleWhenInactive;
-        m_backgroundImage.color = m_imageColorWhenInactive;
-        m_graphicsRectTransform.localScale = m_graphicsScaleWhenInactive;
+        m_animator.Play(m_deactivateAnimationName);
     }
     
     #endregion
+    
 }
